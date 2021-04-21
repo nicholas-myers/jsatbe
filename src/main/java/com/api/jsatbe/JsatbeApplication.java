3dae1cc9d745fbe21c23c3c9be33155d50fcd40a
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2021-04-21 15:03:41 -0700

    initialized new intellij SpringBoot Application

---
+++ src/main/java/com/api/jsatbe/JsatbeApplication.java
@@ -0,0 +1,15 @@
+package com.api.jsatbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsatbeApplication
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(JsatbeApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/api/jsatbe/JsatbeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.api.jsatbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsatbeApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+   }
+
+}
