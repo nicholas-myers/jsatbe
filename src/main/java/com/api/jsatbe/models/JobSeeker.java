94b60af45ff51956deaefeadab040fbdc0028559
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2021-04-21 18:34:20 -0700

    renamed User to JobSeeker for vocabulary consistency

--- src/main/java/com/api/jsatbe/models/JobSeeker.java
+++ src/main/java/com/api/jsatbe/models/JobSeeker.java
@@ -5,14 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
-public class User
+@Table(name = "jobseekers")
+public class JobSeeker
 {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
    private long userid;
 
-   private String username;
+   private String jobseekername;
 
    private String password;
+
+   private List<DailyActivity> dailies = new ArrayList<>();
+
+   private List timesheets = new ArrayList<>();
 }
