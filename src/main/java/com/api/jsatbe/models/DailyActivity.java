6b34cfc250eeb83dccafb9f589ef4025e49ea3d3
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2021-04-21 18:20:40 -0700

    extracted daily activity properties into new Daily Activity model

---
+++ src/main/java/com/api/jsatbe/models/DailyActivity.java
@@ -0,0 +1,17 @@
+package com.api.jsatbe.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DailyActivity
+{
+   private long dailyactivityid;
+
+   private String date;
+
+   private List resources = new ArrayList<>();
+
+   private List contacts = new ArrayList<>();
+
+   private List meetings = new ArrayList<>();
+}
